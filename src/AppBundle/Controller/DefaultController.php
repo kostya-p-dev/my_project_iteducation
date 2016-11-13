7fd89082878aaad567bf8ffb8ec5ff01b7019d8a
Kostya <ermine.kostya@gmail.com>
2016-11-13 23:25:46 +0200

    minor fix

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,8 +13,17 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $lessons = $this->getDoctrine()
+            ->getRepository('AppBundle:Lesson')
+            ->findAll();
+
+        if (!$lessons) {
+            throw $this->createNotFoundException(
+                'No lessons found for id '
+            );
+        }
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('default/index.html.twig', ['lessons' => $lessons,
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
@@ -23,10 +32,7 @@ class DefaultController extends Controller
      */
     public function lessonsAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+
     }
 
 
